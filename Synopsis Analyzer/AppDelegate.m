4038346ef388a3616686396b899ecc5c29b195e9
ray cutler <ray@vidvox.net>
2019-12-17 09:51:46 -0500

    work on analyzer, added new target
    added spotlight importer target back to this project

--- Synopsis Analyzer/AppDelegate.m
+++ Synopsis Analyzer/AppDelegate.m
@@ -296,6 +296,20 @@
 
 - (void) initSpotlight
 {
+	//	we have to 'touch' the directory containing the spotlight importer to get the OS to recognize that it exists and start using it
+	NSBundle	*mb = [NSBundle mainBundle];
+	NSURL		*bundleURL = [mb bundleURL];
+	bundleURL = [bundleURL URLByAppendingPathComponent:@"Contents"];
+	bundleURL = [bundleURL URLByAppendingPathComponent:@"Library"];
+	bundleURL = [bundleURL URLByAppendingPathComponent:@"Spotlight"];
+	
+	NSTask		*touchTask = [[NSTask alloc] init];
+	NSError		*nsErr = nil;
+	[touchTask setExecutableURL:[NSURL fileURLWithPath:@"/usr/bin/touch" isDirectory:NO]];
+	[touchTask setArguments:@[ [NSString stringWithFormat:@"%@",bundleURL.path] ]];
+	[touchTask launchAndReturnError:&nsErr];
+	
+	/*
 	NSURL* spotlightFileURL = nil;
 	NSURL* resourceURL = [[NSBundle mainBundle] resourceURL];
 	
@@ -323,6 +337,7 @@
 		
 		[exampleValues writeToFile:[spotlightFileURL path] atomically:YES];
 	}
+	*/
 }
 
 
